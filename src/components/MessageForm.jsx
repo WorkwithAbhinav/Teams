44b50e23efedb8962953afe549a36525057ae492
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2023-11-03 11:09:17 +0530

    Teams

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,21 +1,59 @@
-const MyMessage = ({ message }) => {
-    if (message.attachments && message.attachments.length > 0) {
-      return (
-        <img
-          src={message.attachments[0].file}
-          alt="message-attachment"
-          className="message-image"
-          style={{ float: 'right' }}
-        />
-      );
-    }
-  
-    return (
-      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
-        {message.text}
-      </div>
-    );
+import { useState } from 'react';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
+
+const MessageForm = (props) => {
+  const [value, setValue] = useState('');
+  const { chatId, creds } = props;
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
   };
-  
-  export default MyMessage;
-  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+    }
+
+    setValue('');
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: '' });
+  };
+
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: 'none' }}
+        onChange={handleUpload.bind(this)}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
+    </form>
+  );
+};
+
+export default MessageForm;
